f942e1786ae0590afc2032a735b99bbd62c7c0b7
elm0n0 <juanjo.perez.plaza@gmail.com>
2018-12-06 15:02:03 +0100

    no message

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/'));
+app.use(express.static(__dirname + '../'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join('/index.html'));
+app.get('*', function(req,res) {
+res.sendFile(path.join(__dirname+'../index.html'));
 });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+console.log("server started");
