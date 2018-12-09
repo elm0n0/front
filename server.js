846a6d1593493ce791abb06fbb28c0fb1ea4e068
elm0n0 <juanjo.perez.plaza@gmail.com>
2018-12-09 14:39:22 +0100

    version funcional en heraku o eso creo...

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.use(express.static(__dirname + '/dist/my-web-app'));
 app.get('/*', function(req,res) {  
     res.sendFile(path.join(__dirname+'/dist/my-web-app/index.html'));   
 });  
-
+    
 // Start the app by listening on the default Heroku port    
 app.listen(process.env.PORT || 8080);
--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -10,20 +10,20 @@ import { UserModel } from './../model/usuarios.model';
 
 export class UserService {
   usuarios: Array<UserModel>;
-  service_get = 'getUser';
-  service_delete = 'deleteUser';
+  service_get = 'https://my-socialweb-app.herokuapp.com/getUser';
+  service_delete = 'https://my-socialweb-app.herokuapp.com/deleteUser';
   constructor(private http: HttpClient) {
     this.usuarios = new Array<UserModel>();
   }
 
   public getUser(): Observable<UserModel[]> {
-    return this.http.get<UserModel[]>(this.service_get);
+    return this.http.get<UserModel[]>('https://cors-elm0n0.herokuapp.com/?q=' + this.service_get);
   }
 
   public delete(): Observable<any> {
     if (sessionStorage.getItem('user')) {
       this.usuarios.push(JSON.parse(sessionStorage.getItem('user')));
-      return this.http.post(this.service_delete, JSON.stringify(this.usuarios));
+      return this.http.post('https://cors-elm0n0.herokuapp.com/?q=' + this.service_delete, JSON.stringify(this.usuarios));
     }
   }
 
